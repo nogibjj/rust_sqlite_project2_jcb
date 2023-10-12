a137cc24e0f78dc28f2d4f68b3c7d64a06c18f79
cervs257 <cervantes257@gmail.com>
2023-10-12 16:55:34 -0400

    modified main to Copilot suggestion

--- polars-hello-world-code-whisperer/src/main.rs
+++ polars-hello-world-code-whisperer/src/main.rs
@@ -2,10 +2,14 @@
 Polars hello world script that uses AWS Code Catalyst and Code Whisperer
 */
 use polars::prelude::*;
+use std::fs::File;
 
 pub fn calculate() -> Result<DataFrame, PolarsError> {
+    // Open the CSV file
+    let file = File::open("data/iris.csv")?;
+
     // Read the CSV data using CsvReader
-    let df = CsvReader::new("data/iris.csv")
+    let df = CsvReader::new(file)
         .has_header(true)
         .finish()?
         .lazy()
