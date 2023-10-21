d4c2cb486e5abdc012bee6d6a4cea3ebf6539b4a
cervs257 <cervantes257@gmail.com>
2023-10-20 23:42:30 -0400

    FinData as a class and main to run are working

--- rust_yahoo_finance/src/lib.rs
+++ rust_yahoo_finance/src/lib.rs
@@ -8,36 +8,48 @@ use polars::prelude::*;
 use tokio_test;
 use yahoo_finance_api as yahoo;
 
-pub fn get_data(ticker: &str, interval: &str, period: &str) -> DataFrame {
-    let provider = yahoo::YahooConnector::new();
-    let response =
-        tokio_test::block_on(provider.get_quote_range(ticker, interval, period)).unwrap();
-    let quotes = response.quotes().unwrap();
-    // Create a DataFrame from the quotes data
-    let df = DataFrame::new(vec![
-        Series::new(
-            "date",
-            &quotes.iter().map(|q| q.timestamp).collect::<Vec<_>>(),
-        ),
-        Series::new("open", &quotes.iter().map(|q| q.open).collect::<Vec<_>>()),
-        Series::new("high", &quotes.iter().map(|q| q.high).collect::<Vec<_>>()),
-        Series::new("low", &quotes.iter().map(|q| q.low).collect::<Vec<_>>()),
-        Series::new("close", &quotes.iter().map(|q| q.close).collect::<Vec<_>>()),
-        Series::new(
-            "adjclose",
-            &quotes.iter().map(|q| q.adjclose).collect::<Vec<_>>(),
-        ),
-        Series::new(
-            "volume",
-            &quotes.iter().map(|q| q.volume).collect::<Vec<_>>(),
-        ),
-    ])
-    .unwrap();
-    df
+pub struct FinData {
+    pub df: DataFrame,
 }
 
-pub fn print_summary(df: &DataFrame) {
-    println!("{}", df);
-    println!("Summary Statistics:");
-    println!("{}", df.describe().unwrap());
+impl FinData {
+    pub fn new(ticker: &str, interval: &str, period: &str) -> Self {
+        let provider = yahoo::YahooConnector::new();
+        let response =
+            tokio_test::block_on(provider.get_quote_range(ticker, interval, period)).unwrap();
+        let quotes = response.quotes().unwrap();
+        // Create a DataFrame from the quotes data
+        let df = DataFrame::new(vec![
+            Series::new(
+                "date",
+                &quotes.iter().map(|q| q.timestamp).collect::<Vec<_>>(),
+            ),
+            Series::new("open", &quotes.iter().map(|q| q.open).collect::<Vec<_>>()),
+            Series::new("high", &quotes.iter().map(|q| q.high).collect::<Vec<_>>()),
+            Series::new("low", &quotes.iter().map(|q| q.low).collect::<Vec<_>>()),
+            Series::new("close", &quotes.iter().map(|q| q.close).collect::<Vec<_>>()),
+            Series::new(
+                "adjclose",
+                &quotes.iter().map(|q| q.adjclose).collect::<Vec<_>>(),
+            ),
+            Series::new(
+                "volume",
+                &quotes.iter().map(|q| q.volume).collect::<Vec<_>>(),
+            ),
+        ])
+        .unwrap();
+        Self { df }
+    }
+    // print summary statistics for the dataframe similar to pandas describe()
+    pub fn print_summary(&self) {
+        println!("{}", self.df);
+        println!("Summary Statistics:");
+        println!("{}", self.df.describe(None).unwrap());
+    }
+
+    // pub fn print_summary(&self) {
+    //     println!("{}", self.df);
+    //     println!("Summary Statistics:");
+    //     println!("{}", self.df.describe().unwrap());
+    // }
 }
--- rust_yahoo_finance/src/main.rs
+++ rust_yahoo_finance/src/main.rs
@@ -1,33 +1,48 @@
-use polars::prelude::*;
-// use std::time::{Duration, UNIX_EPOCH};
-use tokio_test;
-use yahoo_finance_api as yahoo;
+/* Command-line interface for FinData */
+use clap::Parser;
+
+// Command-line interface for FinData
+
+#[derive(Parser)]
+#[clap(
+    author = "Javier Cervantes",
+    version = "v1.0",
+    about = "Command-line interface for FinData"
+)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Parser)]
+enum Commands {
+    #[clap(name = "summary")]
+    Summary(SummaryCommand),
+}
+
+#[derive(Parser)]
+struct SummaryCommand {
+    #[clap(short, long)]
+    ticker: String,
+    #[clap(short, long)]
+    interval: String,
+    #[clap(short, long)]
+    period: String,
+}
 
 fn main() {
-    let provider = yahoo::YahooConnector::new();
-    let response = tokio_test::block_on(provider.get_quote_range("AAPL", "1d", "1mo")).unwrap();
-    let quotes = response.quotes().unwrap();
-    // Create a DataFrame from the quotes data
-    let df = DataFrame::new(vec![
-        Series::new(
-            "date",
-            &quotes.iter().map(|q| q.timestamp).collect::<Vec<_>>(),
-        ),
-        Series::new("open", &quotes.iter().map(|q| q.open).collect::<Vec<_>>()),
-        Series::new("high", &quotes.iter().map(|q| q.high).collect::<Vec<_>>()),
-        Series::new("low", &quotes.iter().map(|q| q.low).collect::<Vec<_>>()),
-        Series::new("close", &quotes.iter().map(|q| q.close).collect::<Vec<_>>()),
-        Series::new(
-            "adjclose",
-            &quotes.iter().map(|q| q.adjclose).collect::<Vec<_>>(),
-        ),
-        Series::new(
-            "volume",
-            &quotes.iter().map(|q| q.volume).collect::<Vec<_>>(),
-        ),
-    ])
-    .unwrap();
-
-    // Print the DataFrame
-    println!("{}", df);
+    let cli = Cli::parse();
+    match cli.command {
+        Some(Commands::Summary(summary_command)) => {
+            let fin_data = yahoo_finance_demo::FinData::new(
+                &summary_command.ticker,
+                &summary_command.interval,
+                &summary_command.period,
+            );
+            fin_data.print_summary();
+        }
+        None => {
+            println!("No subcommand specified");
+        }
+    }
 }
